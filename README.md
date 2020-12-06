80273785b45741cddd9651d07545c9b123c2bde3
Tomasz Dłuski <mr.toumash@gmail.com>
2020-12-06 15:08:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Endomondo data exporter
+
+How to export data from endomondo?
+
+1. Enter your endomondo login/password into the `settings.json` file
+2. Create a directory named `tmp` in the run directory
+2. Run `node index.js` and watch the results
---
+++ index.js
@@ -0,0 +1,28 @@
+require('cross-fetch/polyfill');
+const { Api, MobileApi } = require('endomondo-api-handler');
+const fs = require('fs');
+const cfg = require('./settings.json')
+
+const api = new Api();
+const mobileApi = new MobileApi();
+
+var login = cfg.login;
+var password = cfg.password;
+
+(async () => {
+    await Promise.all([
+        api.login(login, password),
+        mobileApi.login(login, password),
+    ]);
+
+    console.log(await api.get('rest/session'));
+
+    await api.processWorkouts({}, async (workout) => {
+        console.log(workout.toString());
+        if (workout.hasGPSData()) {
+            var type = workout.getTypeName();
+            fs.writeFileSync(`tmp/${type}_${workout.getId()}.gpx`, await api.getWorkoutGpx(workout.getId()), 'utf8');
+        }
+    });
+
+})();
